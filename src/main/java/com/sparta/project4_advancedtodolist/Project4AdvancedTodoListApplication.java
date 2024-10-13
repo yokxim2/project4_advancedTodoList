d2a0763f1358846bc4a1d0e93dd6152dadf4b62b
yokxim <yooksi53@gmail.com>
2024-10-13 11:33:16 +0900

    Initial commit

---
+++ src/main/java/com/sparta/project4_advancedtodolist/Project4AdvancedTodoListApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.project4_advancedtodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project4AdvancedTodoListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Project4AdvancedTodoListApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/project4_advancedtodolist/Project4AdvancedTodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.project4_advancedtodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project4AdvancedTodoListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
