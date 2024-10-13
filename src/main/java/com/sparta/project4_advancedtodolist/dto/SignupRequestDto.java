8a2f3ca33d2c6065604c1ea0db98ad01ed724d7d
yokxim <yooksi53@gmail.com>
2024-10-13 14:11:11 +0900

    initial dto for signup

---
+++ src/main/java/com/sparta/project4_advancedtodolist/dto/SignupRequestDto.java
@@ -0,0 +1,25 @@
+package com.sparta.project4_advancedtodolist.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SignupRequestDto {
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    private String password;
+
+    @Pattern(regex = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
+    @NotBlank
+    private String email;
+
+    private boolean isAdmin = false;
+
+    private String adminToken = "";
+}
