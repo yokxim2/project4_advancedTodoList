7b78df3a4ae652d98b5350f047b5bbd0f6f1fa8d
yokxim <yooksi53@gmail.com>
2024-10-15 17:08:17 +0900

    change URL, add @PathVariable annotation to use todoId in API methods

--- src/main/java/com/sparta/project4_advancedtodolist/controller/CommentController.java
+++ src/main/java/com/sparta/project4_advancedtodolist/controller/CommentController.java
@@ -3,6 +3,7 @@ package com.sparta.project4_advancedtodolist.controller;
 import com.sparta.project4_advancedtodolist.dto.CommentRequestDto;
 import com.sparta.project4_advancedtodolist.dto.CommentResponseDto;
 import com.sparta.project4_advancedtodolist.service.CommentService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -11,24 +12,24 @@ import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/api/todos/{todoId}")
 public class CommentController {
 
     private final CommentService commentService;
 
     @PostMapping("/comments")
-    public CommentResponseDto addComment(@RequestBody CommentRequestDto requestDto) {
-        return commentService.addComment(requestDto);
+    public CommentResponseDto addComment(@PathVariable Long todoId, @Valid CommentRequestDto requestDto) {
+        return commentService.addComment(todoId, requestDto);
     }
 
     @GetMapping("/comments")
-    public List<CommentResponseDto> getComments() {
-        return commentService.getComments();
+    public List<CommentResponseDto> getComments(@PathVariable Long todoId) {
+        return commentService.getComments(todoId);
     }
 
     @PutMapping("/comments/{commentId}")
-    public CommentResponseDto updateComment(@PathVariable Long id, @RequestBody CommentRequestDto requestDto) {
-        return commentService.updateComment(id, requestDto);
+    public CommentResponseDto updateComment(@PathVariable Long todoId, @PathVariable Long commentId, @RequestBody CommentRequestDto requestDto) {
+        return commentService.updateComment(todoId, commentId, requestDto);
     }
 
     @DeleteMapping("/comments/{commentId}")
