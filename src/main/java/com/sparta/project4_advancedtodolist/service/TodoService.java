775600c63d4ef7e6b8bd4f1405cfef9820ce552d
yokxim <yooksi53@gmail.com>
2024-10-16 17:54:55 +0900

    advanced password logic

--- src/main/java/com/sparta/project4_advancedtodolist/service/TodoService.java
+++ src/main/java/com/sparta/project4_advancedtodolist/service/TodoService.java
@@ -1,13 +1,15 @@
 package com.sparta.project4_advancedtodolist.service;
 
-import com.sparta.project4_advancedtodolist.dto.ManagerRequestDto;
-import com.sparta.project4_advancedtodolist.dto.TodoRequestDto;
-import com.sparta.project4_advancedtodolist.dto.TodoResponseDto;
+import com.sparta.project4_advancedtodolist.dto.todo.ManagerRequestDto;
+import com.sparta.project4_advancedtodolist.dto.todo.PasswordRequiredTodoRequestDto;
+import com.sparta.project4_advancedtodolist.dto.todo.TodoRequestDto;
+import com.sparta.project4_advancedtodolist.dto.todo.TodoResponseDto;
 import com.sparta.project4_advancedtodolist.entity.Todo;
 import com.sparta.project4_advancedtodolist.entity.User;
 import com.sparta.project4_advancedtodolist.entity.UserTodo;
 import com.sparta.project4_advancedtodolist.repository.TodoRepository;
 import com.sparta.project4_advancedtodolist.repository.UserTodoRepository;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -15,6 +17,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -43,6 +47,7 @@ public class TodoService {
     @Transactional
     public void addManager(Long todoId, ManagerRequestDto requestDto) {
         Todo existTodo = findTodoById(todoId);
+        checkCreatorPassword(existTodo, requestDto.getPassword());
         User existUser = userService.findUserByUsername(requestDto.getUsername());
 
         userTodoRepository.save(UserTodo.makeUserTodo(existUser, existTodo));
@@ -58,19 +63,58 @@ public class TodoService {
     }
 
     @Transactional
-    public TodoResponseDto updateTodo(Long id, TodoRequestDto requestDto) {
+    public TodoResponseDto updateTodo(Long id, @Valid PasswordRequiredTodoRequestDto requestDto) {
         Todo existTodo = findTodoById(id);
+        User editor = userService.findUserById(requestDto.getUserId());
+
+        checkEditorPermission(existTodo, editor, requestDto.getPassword());
+
         existTodo.update(requestDto.getTitle(), requestDto.getContent());
         return new TodoResponseDto(existTodo);
     }
 
     @Transactional
-    public void deleteTodo(Long id) {
+    public void deleteTodo(Long id, @Valid PasswordRequiredTodoRequestDto requestDto) {
         Todo existTodo = findTodoById(id);
+        checkCreatorPassword(existTodo, requestDto.getPassword());
+
         todoRepository.delete(existTodo);
     }
 
     // ==== 편의 메서드 ====
+
+    @Transactional
+    // 일정을 삭제하려고 할 때, 혹은 일정 수정 권한을 공유 기능은 일정 작성자의 권한이다.
+    public void checkCreatorPassword(Todo existTodo, String previousPassword) {
+        UserTodo matchedUserTodo = existTodo.getUserTodoList().stream()
+                .filter(userTodo -> userTodo.getTodo().equals(existTodo))
+                .findFirst()
+                .orElseThrow(
+                        () -> new IllegalArgumentException("해당 Todo에 연결된 유저가 없습니다.")
+                );
+
+        User user = matchedUserTodo.getUser();
+        if (!user.getPassword().equals(previousPassword)) {
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+    }
+
+    @Transactional
+    // 일정을 수정하려고 할 때 수정 권한이 있는지 확인 후 비밀번호를 검증한다.
+    public void checkEditorPermission(Todo existTodo, User editor, String password) {
+        List<User> users = existTodo.getUserTodoList().stream()
+                .map(UserTodo::getUser)
+                .toList();
+
+        if (!users.contains(editor)) {
+            throw new IllegalArgumentException("해당 일정에 대한 수정 권한이 없습니다.");
+        }
+
+        if (!editor.getPassword().equals(password)) {
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+    }
+
     @Transactional
     public Todo findTodoById(Long id) {
         Todo todo = todoRepository.findById(id).orElseThrow(
