06e092b52b23696afdc485c71f2901853d423674
yokxim <yooksi53@gmail.com>
2024-10-16 17:52:10 +0900

    todoList will show username info from now on

--- src/main/java/com/sparta/project4_advancedtodolist/dto/comment/CommentResponseDto.java
+++ src/main/java/com/sparta/project4_advancedtodolist/dto/comment/CommentResponseDto.java
@@ -1,21 +1,21 @@
-package com.sparta.project4_advancedtodolist.dto;
+package com.sparta.project4_advancedtodolist.dto.comment;
 
 import com.sparta.project4_advancedtodolist.entity.Comment;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-
 @Getter
 @NoArgsConstructor
 public class CommentResponseDto {
     private Long id;
+    private String username;
     private String content;
     private String createdAt;
     private String modifiedAt;
 
     public CommentResponseDto(Comment comment) {
         this.id = comment.getId();
+        this.username = comment.getUser().getUsername();
         this.content = comment.getContent();
         this.createdAt = comment.getCreatedAt().toString();
         this.modifiedAt = comment.getModifiedAt().toString();
